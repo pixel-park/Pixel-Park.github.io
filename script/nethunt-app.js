a062422417a8547073338fa73bc0f6c050934c8d
pixel-park <pixel-park@users.noreply.github.com>
2019-12-21 05:43:22 +0200

    Update nethunt-app.js

--- script/nethunt-app.js
+++ script/nethunt-app.js
@@ -1,45 +1,55 @@
-let requestURL = 'https://jsonplaceholder.typicode.com/posts';
-const doc = document;
-const email = doc.getElementById("mail");
-const name = doc.getElementById("name");
-const company = doc.getElementById("company");
-const form = doc.getElementById("form");
-const input = doc.querySelectorAll('input');
+"use strict";
 
-let myData = {
-  name : 'none',
-  email : 'none',
-  company : 'none'
-}; 
+var requestURL = 'https://jsonplaceholder.typicode.com/posts';
+var doc = document;
+var email = doc.getElementById("mail");
+var name = doc.getElementById("name");
+var company = doc.getElementById("company");
+var form = doc.getElementById("form");
+
+var myData = {
+  name: 'none',
+  email: 'none',
+  company: 'none'
+};
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    if (name.value.length < 4) {
-        name.classList.add("bad");  
-    } else {name.classList.remove("bad");
-    myData.name = name.value;};
-    if (!email.validity.valid) {
-    email.classList.add("bad"); 
-    } else {email.classList.remove("bad");
-    myData.email = email.value;};
-    if (company.value.length < 4) {
-   company.classList.add("bad");   
-  } else {company.classList.remove("bad");
-  myData.company = company.value;}
- 
- if (!name.classList.contains('bad') && !email.classList.contains('bad') && !company.classList.contains('bad')){setTimeout(function(){
-      
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', requestURL);
-    xhr.send(JSON.stringify(myData));
-  
-    console.log(JSON.stringify(myData));
-    alert(`Спасибо!\nВаше имя ${myData.name}\nВаш Email ${myData.email} и ваша компания ${myData.company}`);
-    form.reset()
-  },200)} 
-  }, false); 
- /*----------------------------------navigation--------------------------*/
+  event.preventDefault();
+  if (name.value.length < 4) {
+    name.classList.add("bad");
+  } else {
+    name.classList.remove("bad");
+    myData.name = name.value;
+  };
+  if (!email.validity.valid) {
+    email.classList.add("bad");
+  } else {
+    email.classList.remove("bad");
+    myData.email = email.value;
+  };
+  if (company.value.length < 4) {
+    company.classList.add("bad");
+  } else {
+    company.classList.remove("bad");
+    myData.company = company.value;
+  }
 
- function todl(){window.scroll ({top:320, left:0, behavior: 'smooth'});
-  window.scroll (0, 320);};
- 
+  if (!name.classList.contains('bad') && !email.classList.contains('bad') && !company.classList.contains('bad')) {
+    setTimeout(function () {
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', requestURL);
+      xhr.send(JSON.stringify(myData));
+
+      console.log(JSON.stringify(myData));
+      alert("\u0421\u043F\u0430\u0441\u0438\u0431\u043E!\n\u0412\u0430\u0448\u0435 \u0438\u043C\u044F " + myData.name + "\n\u0412\u0430\u0448 Email " + myData.email + " \u0438 \u0432\u0430\u0448\u0430 \u043A\u043E\u043C\u043F\u0430\u043D\u0438\u044F " + myData.company);
+      form.reset();
+    }, 200);
+  }
+}, false);
+/*----------------------------------navigation--------------------------*/
+
+function todl() {
+  window.scroll({ top: 320, left: 0, behavior: 'smooth' });
+  window.scroll(0, 320);
+};
